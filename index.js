12819230b656af9988c5595ca25e006c31715cfd
WillieWoo257986 <williamkulcsar@gmail.com>
2020-05-31 21:00:00 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzE0NDA5OTUzMDM2NzMwMzkw.Xsw_Og.bPwnwGzT4w0oebSacmQ7lBmgSqg';
+const token = '';
 
 const CommandsChannel = 'commands';
 const PingPongChannel = 'ping-pong';
